9ab80d76ebe262c94a27c06cff8f6cb173c54d49
Ben Firshman <ben@firshman.co.uk>
2017-08-03 14:39:33 +0300

    Add output directory option

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var childProcess = require("child_process");
 var distill = require("distill-template");
-var fs = require("fs");
+var fs = require("fs-extra");
 var jsdom = require("jsdom");
 var path = require("path");
 var postprocessors = require("./postprocessor");
+
+var normalizeDirectory = (dir) => {
+  if (dir.slice(-1) == "/") {
+    dir = dir.slice(0, -1);
+  }
+  return path.normalize(dir);
+}
+
+var prepareOutputDir = (inputDir, outputDir, callback) => {
+  // Nothing to prepare
+  if (inputDir === outputDir) {
+    return callback();
+  }
+  fs.copy(inputDir, outputDir, callback);
+};
 
 // Render a LaTeX document with Pandoc. Callback is called with error or
 // path to directory with index.html in it.
 exports.renderPandoc = (texPath, callback) => {
   var outputDir = path.dirname(texPath);
+  var texFilename = path.basename(texPath);
   var htmlPath = path.join(outputDir, "index.html");
   var args = [
     "--from",
@@ -20,10 +36,12 @@ exports.renderPandoc = (texPath, callback) => {
     "--filter",
     "/app/pandocfilter/engrafo_pandocfilter.py",
     "--output",
-    htmlPath,
-    texPath
+    "index.html",
+    texFilename
   ];
-  var pandoc = childProcess.spawn("/usr/local/bin/pandoc", args);
+  var pandoc = childProcess.spawn("/usr/local/bin/pandoc", args, {
+    cwd: outputDir,
+  });
   var stdout = "";
   var stderr = "";
   pandoc.stdout.on("data", data => {
@@ -62,16 +80,25 @@ exports.postprocess = htmlString => {
   return jsdom.serializeDocument(dom);
 };
 
-// Render and postprocess a LaTeX file. Calls callback with an error on failure
-// or a path to an HTML file on success.
-exports.render = (texPath, callback) => {
-  exports.renderPandoc(texPath, (err, htmlPath) => {
-    if (err) return callback(err, htmlPath);
-    fs.readFile(htmlPath, "utf8", (err, htmlString) => {
+// Render and postprocess a LaTeX file into outputDir (created if does not
+// exist). Calls callback with an error on failure or a path to an HTML file
+// on success.
+exports.render = (texPath, outputDir, callback) => {
+  var inputDir = normalizeDirectory(path.dirname(texPath));
+  outputDir = normalizeDirectory(outputDir);
+  var texFilename = path.basename(texPath);
+  var outputTexPath = path.join(outputDir, texFilename);
+
+  prepareOutputDir(inputDir, outputDir, (err) => {
+    if (err) return callback(err);
+    exports.renderPandoc(outputTexPath, (err, htmlPath) => {
       if (err) return callback(err, htmlPath);
-      var transformedHtml = exports.postprocess(htmlString);
-      fs.writeFile(htmlPath, transformedHtml, err => {
-        return callback(err, htmlPath);
+      fs.readFile(htmlPath, "utf8", (err, htmlString) => {
+        if (err) return callback(err, htmlPath);
+        var transformedHtml = exports.postprocess(htmlString);
+        fs.writeFile(htmlPath, transformedHtml, err => {
+          return callback(err, htmlPath);
+        });
       });
     });
   });

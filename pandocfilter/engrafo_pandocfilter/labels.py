38f6deacd4b7e38235db0b819adff3bdcb186692
Ben Firshman <ben@firshman.co.uk>
2017-08-07 18:53:21 +0300

    Hide \subsubsection{} labels
    Fixes #47

--- pandocfilter/engrafo_pandocfilter/labels.py
+++ pandocfilter/engrafo_pandocfilter/labels.py
@@ -170,8 +170,8 @@ def insert_section_labels(key, val, fmt, meta):
     if key == 'Header':
         level, attrs, children = val
 
-        # Ignore \paragraph{} and smaller
-        if level >= 4:
+        # Ignore \subsubsection{}, \paragraph{} and smaller
+        if level >= 3:
             return Header(level + 1, attrs, children)
 
         unnumbered = 'unnumbered' in val[1][1]

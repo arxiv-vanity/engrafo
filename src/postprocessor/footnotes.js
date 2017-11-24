4a127ccdbe73c380f17773db0c69085f0944630d
Ben Firshman <ben@firshman.co.uk>
2017-11-24 12:07:33 +0530

    Make footnotes work on latexml

--- src/postprocessor/footnotes.js
+++ src/postprocessor/footnotes.js
@@ -1,11 +1,17 @@
 let utils = require("./utils");
 
 module.exports = function(dom) {
-  elements = dom.querySelectorAll(".engrafo-footnote");
+  var elements = dom.querySelectorAll(".ltx_note_outer");
 
   // If there are footnotes, put the container element in the appendix.
   // Distill will automatically fill it with footnotes.
   if (elements.length > 0) {
+    // Distill does all numbering for us, so remove latexml generated stuff
+    var noteMarks = dom.querySelectorAll(".ltx_note_mark");
+    Array.from(noteMarks).forEach(noteMark => {
+      noteMark.parentNode.removeChild(noteMark);
+    });
+
     var dtAppendix = dom.querySelector("dt-appendix");
     dtAppendix.appendChild(utils.nodeFromString(dom, "<h3>Footnotes</h3>"));
     dtAppendix.appendChild(dom.createElement("dt-fn-list"));

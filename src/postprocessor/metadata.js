58c0723d699d0591d4e97c56fbbd655edf7c9154
Ben Firshman <ben@firshman.co.uk>
2017-11-24 18:38:23 +0530

    Put metadata in right place

--- src/postprocessor/metadata.js
+++ src/postprocessor/metadata.js
@@ -72,9 +72,17 @@ let css = `
 module.exports = function(dom) {
   utils.addStylesheet(dom, css);
 
+  // Put metadata in right place
   let metadata = utils.nodeFromString(dom, '<div class="engrafo-metadata"></div>');
+  let h1 = dom.querySelector("h1");
+  h1.parentNode.insertBefore(metadata, h1.nextSibling);
+
   let authors = dom.querySelector('.ltx_authors');
-  utils.wrapElement(authors, metadata);
+  if (authors) {
+    metadata.appendChild(authors);
+  }
+
+  metadata.appendChild(utils.nodeFromString(dom, '<div class="engrafo-metadata-custom"></div>'));
 
   // Array.from(authors.children).forEach(el => {
   //   // Replace each line inside each span (bit of a hack. hopefully it won't break.)

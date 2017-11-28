38a38760dea5638b75bd71e3732d712e018f4380
Ben Firshman <ben@firshman.co.uk>
2017-11-28 14:27:45 +0530

    Fix breakage if no abstract

--- src/postprocessor/layout.js
+++ src/postprocessor/layout.js
@@ -16,10 +16,10 @@ module.exports = function(dom) {
   );
 
   // Make abstract title same size as everything else
-  utils.replaceAndKeepChildren(
-    dom.querySelector(".ltx_title_abstract"),
-    dom.createElement('h2')
-  );
+  var abstractTitle = dom.querySelector(".ltx_title_abstract");
+  if (abstractTitle) {
+    utils.replaceAndKeepChildren(abstractTitle, dom.createElement('h2'));
+  }
 
   utils.removeAll(dom.querySelector("footer"));
 };

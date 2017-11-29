ea5eeeca2d3baa8227400890ca2d1f02ed5b210f
Ben Firshman <ben@firshman.co.uk>
2017-11-29 10:01:32 +0530

    Remove metadata tests

--- integration-tests/metadata.test.js
+++
@@ -1,5 +0,0 @@
-var utils = require("./utils");
-
-test("footnotes after metadata are inlined inside metadata", done => {
-  utils.expectBodyToMatchSnapshot("metadata/footnotes-after-metadata.tex", done);
-});

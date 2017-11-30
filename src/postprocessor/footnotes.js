3d08f0b2c6b4f02eea14d1c267d5506d4e95542b
Ben Firshman <ben@firshman.co.uk>
2017-11-30 12:20:48 +0530

    Remove unnecessary element from footnotes

--- src/postprocessor/footnotes.js
+++ src/postprocessor/footnotes.js
@@ -1,21 +1,22 @@
 let utils = require("./utils");
 
 module.exports = function(dom) {
-  var elements = dom.querySelectorAll(".ltx_note_outer");
+  var elements = dom.querySelectorAll(".ltx_note");
 
-  // If there are footnotes, put the container element in the appendix.
-  // Distill will automatically fill it with footnotes.
-  if (elements.length > 0) {
-    // Distill does all numbering for us, so remove latexml generated stuff
-    var noteMarks = dom.querySelectorAll(".ltx_note_mark");
-    Array.from(noteMarks).forEach(noteMark => {
-      noteMark.parentNode.removeChild(noteMark);
-    });
-
-    var dtAppendix = dom.querySelector("dt-appendix");
-    dtAppendix.appendChild(utils.nodeFromString(dom, "<h3>Footnotes</h3>"));
-    dtAppendix.appendChild(dom.createElement("dt-fn-list"));
+  // No footnotes, don't do anything!
+  if (elements.length == 0) {
+    return;
   }
+
+  // Distill does all numbering for us, so remove latexml generated stuff
+  var noteMarks = dom.querySelectorAll(".ltx_note_mark");
+  Array.from(noteMarks).forEach(noteMark => {
+    noteMark.parentNode.removeChild(noteMark);
+  });
+
+  var dtAppendix = dom.querySelector("dt-appendix");
+  dtAppendix.appendChild(utils.nodeFromString(dom, "<h3>Footnotes</h3>"));
+  dtAppendix.appendChild(dom.createElement("dt-fn-list"));
 
   // Replace footnotes with Distill footnotes
   Array.from(elements).forEach(span => {

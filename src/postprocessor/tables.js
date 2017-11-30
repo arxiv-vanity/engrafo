80ea69acc8759d6812fda1bacfdbdd6a52626f3f
Ben Firshman <ben@firshman.co.uk>
2017-11-30 12:42:09 +0530

    Always put table captions underneath

--- src/postprocessor/tables.js
+++ src/postprocessor/tables.js
@@ -1,4 +1,10 @@
 let utils = require("./utils");
 
 module.exports = function(dom) {
+  Array.from(dom.querySelectorAll("figure.ltx_table")).forEach(figure => {
+    // If caption is at start, put it at end
+    if (figure.children[0].tagName == "FIGCAPTION") {
+      figure.appendChild(figure.children[0]);
+    }
+  });
 };

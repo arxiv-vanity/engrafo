b372199b148f987a348666e8f9da2257301e21dd
Ben Firshman <ben@firshman.co.uk>
2017-12-01 19:11:15 +0530

    Make authors bold
    Kinda. And remove a lot of old metadata styles.

--- src/postprocessor/metadata.js
+++ src/postprocessor/metadata.js
@@ -25,48 +25,13 @@ let css = `
   white-space: normal;
 }
 
-.engrafo-metadata > .authors {
-}
-
-.engrafo-metadata .author {
-  margin-bottom: 12px;
-}
-
-/* HACK(bfirsh): this seems to make things render a bit better. but really this is just making up for our dodgy authors output */
-.engrafo-metadata .author > span,
-.engrafo-metadata .author .thanks {
-  display: block;
-}
-
-.engrafo-metadata .author > span:first-child {
+.engrafo-metadata:first-line {
   font-weight: bold;
-}
-
-.engrafo-metadata .author > span:first-child .thanks {
-  font-weight: normal;
 }
 
 .engrafo-metadata .engrafo-metadata-custom {
   color: rgba(0, 0, 0, 0.5);
 }
-
-/* DISABLED: put the authors on the RHS
-@media(min-width: 768px) {
-  .engrafo-metadata {
-    display: flex;
-  }
-  .engrafo-metadata > .authors {
-    flex: 2;
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
-    padding-right: 30px;
-    margin-right: 15px;
-  }
-  .engrafo-metadata .engrafo-metadata-custom {
-    flex: 1;
-  }
-}
-*/
-
 `;
 
 module.exports = function(dom) {

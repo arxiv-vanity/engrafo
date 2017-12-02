33a0dcea7f72f2b8ad9bb12977244188d6867900
Ben Firshman <ben@firshman.co.uk>
2017-12-02 09:01:20 +0530

    Move headings integration test

--- integration-tests/headings.test.js
+++ integration-tests/headings.test.js
@@ -1,5 +1,9 @@
 var utils = require("./utils");
 
+test("basic titles and headings render correctly", done => {
+  utils.expectBodyToMatchSnapshot("headings/basic.tex", done);
+});
+
 test("bold is stripped from title", done => {
   utils.expectBodyToMatchSnapshot("headings/bold-title.tex", done);
 });
--- integration-tests/layout.test.js
+++ integration-tests/layout.test.js
@@ -4,10 +4,6 @@ test("authors render correctly", done => {
   utils.expectBodyToMatchSnapshot("layout/authors.tex", done);
 });
 
-test("titles and headings render correctly", done => {
-  utils.expectBodyToMatchSnapshot("layout/headings.tex", done);
-});
-
 test("paragraphs and line breaks render correctly", done => {
   utils.expectBodyToMatchSnapshot("layout/paragraphs.tex", done);
 });

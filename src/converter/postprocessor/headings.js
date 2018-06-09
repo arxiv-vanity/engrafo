b6299cc4be9dd298260b8da08a6af3ac7d5dac17
Ben Firshman <ben@firshman.co.uk>
2018-06-09 16:10:59 +0300

    Remove all post-processors

--- src/converter/postprocessor/headings.js
+++ src/converter/postprocessor/headings.js
@@ -1,93 +1,9 @@
-let utils = require("./utils");
 let toTitleCase = require("titlecase");
 
-var css = `
-dt-article h2,
-dt-article h3,
-dt-article h4 {
-  position: relative;
-  /* Space for section number */
-  padding-right: 40px;
-}
-
-@media(min-width: 768px) {
-  dt-article h2,
-  dt-article h3,
-  dt-article h4 {
-    padding-right: 0;
-  }
-}
-
-dt-article .ltx_tag_section,
-dt-article .ltx_tag_subsection {
-  font-family: Baskerville, Georgia, serif;
-  font-style: normal;
-  color: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  width: 150px;
-  right: 0;
-  text-align: right;
-  font-size: 0.9em;
-}
-
-dt-article .ltx_tag_section a,
-dt-article .ltx_tag_subsection a,
-dt-article .ltx_tag_section a:active,
-dt-article .ltx_tag_subsection a:active {
-  border-bottom: none;
-}
-
-dt-article .ltx_tag_section a:hover,
-dt-article .ltx_tag_subsection a:hover {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-}
-
-@media(min-width: 768px) {
-  dt-article .ltx_tag_section,
-  dt-article .ltx_tag_subsection {
-    right: unset;
-    left: -170px;
-  }
-}
-
-@media (min-width: 1080px) {
-  dt-article .ltx_tag_section,
-  dt-article .ltx_tag_subsection {
-    right: unset;
-    left: -180px;
-  }
-}
-
-dt-article .paragraph-heading {
-  margin-right: 0.5em;
-}
-`;
-
 module.exports = function(dom) {
-  utils.addStylesheet(dom, css);
-
-  // Remove numbers from subsubsection (h4)
-  Array.from(dom.querySelectorAll(".ltx_tag_subsubsection")).forEach(tag => {
-    tag.parentNode.removeChild(tag);
-  });
-
   // Things to apply to all headings
   let headings = dom.querySelectorAll("h1, h2, h3, h4, h5, h6");
   Array.from(headings).forEach(heading => {
-    // Remove .ltx_font_bold and weird styles
-    heading.className = heading.className.replace("ltx_font_bold", "");
-    heading.removeAttribute("style");
-
-    // Remove new lines
-    Array.from(heading.children).forEach(child => {
-      if (child.tagName === "BR") {
-        heading.removeChild(child);
-      }
-    });
-
-    // We've removed a bunch of elements so text nodes might be fragmented
-    heading.normalize();
-
     var textNodes = dom.createTreeWalker(
       heading,
       dom.defaultView.NodeFilter.SHOW_TEXT
@@ -102,35 +18,6 @@ module.exports = function(dom) {
 
       // Clean up weird characters
       node.nodeValue = node.nodeValue.replace("\u3000", "");
-    }
-  });
-
-  // Add section numnbers
-  let sectionNumbers = dom.querySelectorAll(".ltx_tag_section");
-  Array.from(sectionNumbers).forEach(span => {
-    let a = utils.nodeFromString(dom, "<a></a>");
-    a.href = "#" + span.parentNode.parentNode.id;
-    utils.moveChildren(span, a);
-    span.appendChild(a);
-  });
-
-  // Put <h5> and <h6> in front of next paragraph, if it exists.
-  var paragraphHeadings = Array.from(dom.querySelectorAll("h5"));
-  paragraphHeadings = paragraphHeadings.concat(dom.querySelectorAll("h6"));
-  paragraphHeadings.forEach(heading => {
-    var div = heading.nextElementSibling;
-    if (!div) return;
-    var p = div.children[0];
-    if (p && p.tagName == "P") {
-      // Put at start of <p>
-      p.insertBefore(dom.createTextNode(" "), p.firstChild);
-      p.insertBefore(heading, p.firstChild);
-      // Turn it into <strong>
-      var newHeading = utils.nodeFromString(
-        dom,
-        '<strong class="paragraph-heading"></strong>'
-      );
-      utils.replaceAndKeepChildren(heading, newHeading);
     }
   });
 };
--- src/converter/postprocessor/index.js
+++ src/converter/postprocessor/index.js
@@ -1,17 +1,6 @@
 module.exports = {
-  appendix: require("./appendix"),
-  bibliography: require("./bibliography"),
-  code: require("./code"),
-  container: require("./container"),
   figures: require("./figures"),
-  footnotes: require("./footnotes"),
   headings: require("./headings"),
-  layout: require("./layout"),
   links: require("./links"),
-  lists: require("./lists"),
-  math: require("./math"),
-  metadata: require("./metadata"),
-  styles: require("./styles"),
-  tables: require("./tables"),
-  typeset: require("./typeset")
+  math: require("./math")
 };

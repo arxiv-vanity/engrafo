9a7b2a72d33b5e1923d2df22f4f02e7846bc6865
Ben Firshman <ben@firshman.co.uk>
2018-06-14 14:13:45 +0300

    Split out integration tests into separate files
    The tests were running incredinly slow, due to
    https://github.com/americanexpress/jest-image-snapshot/issues/28 .
    
    This also gives the advantage of not producing a new huge image
    in our Git repo every time there is a minor change.

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,12 +1,44 @@
 const utils = require("./utils");
 
-test(
-  "basic latex features render correctly",
-  async () => {
-    await utils.expectToMatchSnapshot("documents/basic.tex");
-  },
-  25000
-);
+test("titles and sections render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/sections.tex");
+});
+
+test("text renders correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/text.tex");
+});
+
+test("paragraph formatting renders correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/paragraph.tex");
+});
+
+test("URLS render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/urls.tex");
+});
+
+test("listings render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/listings.tex");
+});
+
+test("tables render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/tables.tex");
+});
+
+test("figures render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/figures.tex");
+});
+
+test("math renders correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/math.tex");
+});
+
+test("footnotes render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/footnotes.tex");
+});
+
+test("citations render correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/citations.tex");
+});
 
 test(
   "sample2e.tex sample document renders correctly",
@@ -16,10 +48,6 @@ test(
   10000
 );
 
-test(
-  "small2e.tex sample document renders correctly",
-  async () => {
-    await utils.expectToMatchSnapshot("documents/small2e.tex");
-  },
-  10000
-);
+test("small2e.tex sample document renders correctly", async () => {
+  await utils.expectToMatchSnapshot("documents/small2e.tex");
+});

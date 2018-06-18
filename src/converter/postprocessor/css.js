846f80c82ca30fae91113ecd65a12d512c8782b3
Ben Firshman <ben@firshman.co.uk>
2018-06-18 16:32:34 +0300

    Add viewport <meta>

--- src/converter/postprocessor/css.js
+++ src/converter/postprocessor/css.js
@@ -1,8 +1,14 @@
 const utils = require("./utils");
 
 module.exports = function(dom, { externalCSS }) {
+  const head = dom.querySelector("head");
+  const meta = utils.nodeFromString(
+    dom,
+    '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+  );
+  head.appendChild(meta);
+
   if (externalCSS) {
-    const head = dom.querySelector("head");
     const link = utils.nodeFromString(
       dom,
       '<link type="text/css" rel="stylesheet">'

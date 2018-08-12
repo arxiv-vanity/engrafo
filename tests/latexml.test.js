c40e2867d76cd86228a8c8d8dc280ad9bde8e284
Ben Firshman <ben@firshman.co.uk>
2018-08-12 17:37:17 +0400

    Add test to ensure latexml fails on broken document

---
+++ tests/latexml.test.js
@@ -0,0 +1,22 @@
+const latexml = require("../src/converter/latexml");
+const path = require("path");
+const tmp = require("tmp-promise");
+
+let outputDir;
+
+beforeAll(async () => {
+  outputDir = await tmp.dir({ unsafeCleanup: true, dir: "/tmp" });
+});
+
+afterAll(async () => {
+  outputDir.cleanup();
+});
+
+test("latexml raises an exception on fatal error", async () => {
+  await expect(
+    latexml.render({
+      texPath: path.join(__dirname, "other-documents/broken.tex"),
+      outputDir: outputDir.path
+    })
+  ).rejects.toThrowError("latexmlc exited with status 1");
+});

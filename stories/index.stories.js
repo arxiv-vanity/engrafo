b79243e8a040b48bd78a52e5787646d2f47bf952
Ben Firshman <ben@firshman.co.uk>
2018-08-30 17:04:04 +0500

    Add pifont.sty test
    ref #421

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -46,7 +46,8 @@ storiesOf("Packages", module)
   .add("color.sty", createStory("color.sty.tex 1"))
   .add("graphicx.sty", createStory("graphicx.sty.tex 1"))
   .add("hyperref.sty", createStory("hyperref.sty.tex 1"))
-  .add("listings", createStory("listings.tex 1"));
+  .add("listings", createStory("listings.tex 1"))
+  .add("pifont.sty", createStory("pifont.sty.tex 1"));
 
 storiesOf("Complete documents")
   .add("sample2e", createStory("sample2e.tex 1"))

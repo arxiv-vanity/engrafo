e1978114e782469473f26b0d9a5e8d9d94f153aa
Ben Firshman <ben@firshman.co.uk>
2018-09-01 13:53:12 +0500

    Add supertabular test
    ref #421

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -53,7 +53,8 @@ storiesOf("Packages", module)
   .add("IEEEtran.cls", createStory("IEEEtran.cls.tex 1"))
   .add("listings.sty", createStory("listings.sty.tex 1"))
   .add("pifont.sty", createStory("pifont.sty.tex 1"))
-  .add("report.cls", createStory("report.cls.tex 1"));
+  .add("report.cls", createStory("report.cls.tex 1"))
+  .add("supertabular.sty", createStory("supertabular.sty.tex 1"));
 
 storiesOf("Complete documents")
   .add("sample2e", createStory("sample2e.tex 1"))

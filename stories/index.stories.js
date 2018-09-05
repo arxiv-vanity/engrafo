3e6efe5f2360a3f5eeada41f96fb3a0257b10eb8
Ben Firshman <ben@firshman.co.uk>
2018-09-05 11:02:59 +0500

    Generate storybook from static files
    Using the Jest snapshots meant we didn't get images.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,62 +1,29 @@
 /* eslint-disable no-console, react/button-has-type */
 
-import { document } from "global";
 import { storiesOf } from "@storybook/html";
 
-import * as snapshots from "../tests/__snapshots__/integration.test.js.snap";
+// Webpack doesn't import the CSS and JS referenced in the HTML, so we have
+// to import that manually.
 import "../src/assets/css/index.scss";
 import main from "../src/assets/javascript/main";
 
-function createStory(snapshotName) {
+function createStory(html) {
   return () => {
     const container = document.createElement("div");
-    let html = snapshots[snapshotName];
-    // Jest puts strings inside a quote inside the snapshot. Good grief.
-    // First, remove starting and end quote
-    html = html
-      .trim()
-      .replace(/^"/, "")
-      .replace(/"$/, "");
-    // Then remove all the backslashed quotes
-    html = html.replace(/\\"/g, '"');
     container.innerHTML = html;
     main(container);
     return container;
   };
 }
 
-storiesOf("Basic functionality")
-  .add("Citations", createStory("citations.tex 1"))
-  .add("Figures", createStory("figures.tex 1"))
-  .add("Footnotes", createStory("footnotes.tex 1"))
-  .add("Headings", createStory("headings.tex 1"))
-  .add("Lists", createStory("lists.tex 1"))
-  .add("Math", createStory("math.tex 1"))
-  .add("Paragraph styles", createStory("paragraph.tex 1"))
-  .add("Tables", createStory("tables.tex 1"))
-  .add("Text styles", createStory("text.tex 1"))
-  .add("URLs", createStory("urls.tex 1"));
+const integrationImport = require.context(
+  "../tests/integration-output",
+  true,
+  /\.html/
+);
 
-storiesOf("Packages", module)
-  .add("aa.cls", createStory("aa.cls.tex 1"))
-  .add("acronym.sty", createStory("acronym.sty.tex 1"))
-  .add("algorithm.sty", createStory("algorithm.sty.tex 1"))
-  .add("algorithm2e.sty", createStory("algorithm2e.sty.tex 1"))
-  .add("alltt.sty", createStory("alltt.sty.tex 1"))
-  .add("amsart.cls", createStory("amsart.cls.tex 1"))
-  .add("array.sty", createStory("array.sty.tex 1"))
-  .add("cleveref.sty", createStory("cleveref.sty.tex 1"))
-  .add("color.sty", createStory("color.sty.tex 1"))
-  .add("colortbl.sty", createStory("colortbl.sty.tex 1"))
-  .add("graphicx.sty", createStory("graphicx.sty.tex 1"))
-  .add("hyperref.sty", createStory("hyperref.sty.tex 1"))
-  .add("IEEEtran.cls", createStory("IEEEtran.cls.tex 1"))
-  .add("listings.sty", createStory("listings.sty.tex 1"))
-  .add("longtable.sty", createStory("longtable.sty.tex 1"))
-  .add("pifont.sty", createStory("pifont.sty.tex 1"))
-  .add("report.cls", createStory("report.cls.tex 1"))
-  .add("supertabular.sty", createStory("supertabular.sty.tex 1"));
-
-storiesOf("Complete documents")
-  .add("sample2e", createStory("sample2e.tex 1"))
-  .add("small2e", createStory("small2e.tex 1"));
+const stories = storiesOf("Integration");
+integrationImport.keys().forEach(key => {
+  const name = key.replace(/^\.\//, "").replace(/\/index\.html$/, "");
+  stories.add(name, createStory(integrationImport(key)));
+});
--- tests/integration.test.js
+++ tests/integration.test.js
@@ -7,13 +7,13 @@ let percySnapshots = [],
   outputDir;
 
 beforeAll(async () => {
-  outputDir = await tmp.dir({ unsafeCleanup: true, dir: "/tmp" });
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  outputDir = path.join(__dirname, "integration-output");
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 });
 
 afterAll(async () => {
   if (process.env.PERCY_TOKEN) {
-    const percy = spawn("percy", ["snapshot", outputDir.path]);
+    const percy = spawn("percy", ["snapshot", outputDir]);
     percy.stdout.on("data", d => console.log(d.toString()));
     percy.stderr.on("data", d => console.error(d.toString()));
     await new Promise((resolve, reject) => {
@@ -29,12 +29,11 @@ afterAll(async () => {
   } else {
     console.log("No, PERCY_TOKEN envvar, skipping percy test");
   }
-  outputDir.cleanup();
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 }, 20000); // long timeout to wait for Percy
 
 test.each(integrationDocuments())("%s", async (documentName, documentPath) => {
-  const outputPath = path.join(outputDir.path, path.parse(documentName).name);
+  const outputPath = path.join(outputDir, path.parse(documentName).name);
   // await fs.mkdirs(outputPath);
   const { htmlString, document } = await renderToDom(documentPath, outputPath);
 

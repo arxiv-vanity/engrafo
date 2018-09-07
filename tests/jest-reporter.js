ba108ff5381f397fa033e019dec97ec3c920cc6e
Ben Firshman <ben@firshman.co.uk>
2018-09-07 20:50:05 +0500

    Hide console output when tests pass
    Implementation is due to https://github.com/facebook/jest/issues/4156
    
    This is to get around https://github.com/travis-ci/travis-ci/issues/4704
    but is also much neater.

---
+++ tests/jest-reporter.js
@@ -0,0 +1,34 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+// https://github.com/mozilla/addons-frontend/commit/6bbf218dc3c3872cbe25654cb63ed0dfd367cf00
+const chalk = require("chalk");
+const { getConsoleOutput } = require("jest-util");
+const DefaultReporter = require("jest-cli/build/reporters/default_reporter")
+  .default;
+const getResultHeader = require("jest-cli/build/reporters/get_result_header")
+  .default;
+
+const TITLE_BULLET = chalk.bold("\u25cf ");
+
+// This Jest reporter does not output any console.log except when the tests are
+// failing, see: https://github.com/mozilla/addons-frontend/issues/2980.
+class FingersCrossedReporter extends DefaultReporter {
+  printTestFileHeader(testPath, config, result) {
+    this.log(getResultHeader(result, this._globalConfig, config));
+
+    const consoleBuffer = result.console;
+    const testFailed = result.numFailingTests > 0;
+
+    if (testFailed && consoleBuffer && consoleBuffer.length) {
+      // prettier-ignore
+      this.log(
+        `  ${TITLE_BULLET}Console\n\n${getConsoleOutput(
+          config.cwd,
+          !!this._globalConfig.verbose,
+          consoleBuffer
+        )}`
+      );
+    }
+  }
+}
+
+module.exports = FingersCrossedReporter;

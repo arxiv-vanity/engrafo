4a2a463a152633dd03587058dbe2b63de7ba2e50
Ben Firshman <ben@firshman.co.uk>
2018-12-08 19:33:07 +0200

    Remove done todo

--- src/converter/linkifyArxiv.js
+++ src/converter/linkifyArxiv.js
@@ -4,7 +4,6 @@ const groupedIssueRegex = /arxiv:((\d+)\.(\d+))/gi;
 
 module.exports = function linkifyArxiv(input) {
   return input.replace(groupedIssueRegex, (match, p1) => {
-    // TODO: escape
     return `<a href="https://arxiv.org/abs/${escape(p1)}">${escape(match)}</a>`;
   });
 };

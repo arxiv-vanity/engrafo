ee32b7f499702ad73c96fc75892759d1b67188a0
Ben Firshman <ben@firshman.co.uk>
2019-04-19 11:49:54 -0700

    Match abs/1234.5678 as arxiv IDs

--- src/converter/linkifyArxiv.js
+++ src/converter/linkifyArxiv.js
@@ -1,9 +1,9 @@
 const escape = require("escape-html");
 
-const groupedIssueRegex = /arxiv:((\d+)\.(\d+))/gi;
+const groupedIssueRegex = /(arxiv:|abs\/)((\d+)\.(\d+))/gi;
 
 module.exports = function linkifyArxiv(input) {
-  return input.replace(groupedIssueRegex, (match, p1) => {
-    return `<a href="https://arxiv.org/abs/${escape(p1)}">${escape(match)}</a>`;
+  return input.replace(groupedIssueRegex, (match, _, p2) => {
+    return `<a href="https://arxiv.org/abs/${escape(p2)}">${escape(match)}</a>`;
   });
 };
--- tests/linkifyArxiv.test.js
+++ tests/linkifyArxiv.test.js
@@ -6,4 +6,14 @@ describe("linkifyArxiv", () => {
       'This is <a href="https://arxiv.org/abs/1234.5678">arXiv:1234.5678</a> a sentence'
     );
   });
+  it("converts abs/1234.5678 into a link", () => {
+    expect(linkifyArxiv("This is abs/1234.5678 a sentence")).toEqual(
+      'This is <a href="https://arxiv.org/abs/1234.5678">abs/1234.5678</a> a sentence'
+    );
+  });
+  it("converts abs/1234.5678 into a link when there is a comma after", () => {
+    expect(linkifyArxiv("This is abs/1234.5678, a sentence")).toEqual(
+      'This is <a href="https://arxiv.org/abs/1234.5678">abs/1234.5678</a>, a sentence'
+    );
+  });
 });

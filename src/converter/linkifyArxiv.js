2aea02ef266c02757a7619037e7f557e1d949b52
Ben Firshman <ben@firshman.co.uk>
2019-04-19 14:58:16 -0700

    Fix tests

--- src/converter/linkifyArxiv.js
+++ src/converter/linkifyArxiv.js
@@ -4,7 +4,7 @@ const escape = require("escape-html");
 // - "arxiv", followed by 1-10 characters, then an arxiv ID. This matches things like "arxiv/1234.5678" or "as seen in arXiv report 1234.5678"
 // - "abs/" followed immediately by an arXiv ID
 // See linkifyArxiv.test.js for lots of examples
-const groupedIssueRegex = /(arxiv.{1,10}|abs\/)((\d{4})\.(\d{4}))/gi;
+const groupedIssueRegex = /(arxiv.{1,10}?|abs\/)((\d+)\.(\d+))/gi;
 
 module.exports = function linkifyArxiv(input) {
   return input.replace(groupedIssueRegex, (match, _, p2) => {

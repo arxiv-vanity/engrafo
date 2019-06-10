601ab43a6eda028d25d741785e7daf3089a76a08
Ben Firshman <ben@firshman.co.uk>
2019-06-10 10:17:47 +0100

    Make clicking citation actually go to citation

--- src/assets/javascript/citations.js
+++ src/assets/javascript/citations.js
@@ -10,11 +10,7 @@ function getBibItem(el, ref) {
   return el.querySelector(`*[id='${href.slice(1)}']`);
 }
 
-function createContent(el, ref) {
-  const bibItem = getBibItem(el, ref);
-  if (!bibItem) {
-    return;
-  }
+function createContent(bibItem) {
   const itemContainer = document.createElement("div");
   itemContainer.className = "ltx_bibitem";
   itemContainer.innerHTML = bibItem.innerHTML;
@@ -25,20 +21,28 @@ function createContent(el, ref) {
 export default function render(el) {
   const cites = el.querySelectorAll(".ltx_cite");
   for (let cite of cites) {
-    const refs = cite.querySelectorAll(".ltx_ref");
+    const refs = cite.querySelectorAll("a.ltx_ref");
 
     for (let ref of refs) {
-      const content = createContent(el, ref);
-      if (content) {
-        createTooltip(ref, content);
+      const bibItem = getBibItem(el, ref);
+      if (!bibItem) {
+        continue;
       }
-    }
+      // Create tooltip
+      const content = createContent(bibItem);
+      createTooltip(ref, content);
 
-    for (let ref of refs) {
-      // Make clicking links do nothing
-      ref.addEventListener("click", function(e) {
-        e.preventDefault();
-      });
+      // Make clicking citation actually to citation
+      const link = bibItem.querySelector("a");
+      if (link) {
+        ref.setAttribute("href", link.getAttribute("href"));
+        ref.setAttribute("target", "_blank");
+      } else {
+        ref.addEventListener("click", function(e) {
+          e.preventDefault();
+        });
+        ref.className += " engrafo_not_a_link";
+      }
     }
   }
 }
